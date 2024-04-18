d0d6307e94265fba3eea89635e1f48ab070d2aff
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-04-18 11:01:44 +0530

    :rewind::rewind::rewind: Subarrays Sum Equals To Sum :rewind::rewind::rewind:

--- HashMap/03.SubArraySumEqualsToK.java
+++ HashMap/03.SubArraySumEqualsToK.java
@@ -2,21 +2,24 @@
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
-       int ans = 0;
-       int sum = 0;
-HashMap<Integer, Integer> map = new HashMap<>();
-map.put(sum, 1);
-for(int val: nums) {
-sum += val;
-if(map.containsKey (sum - k)) {
-ans += map.get(sum - k);
-}
-if(map.containsKey (sum)){
-map.put(sum, map.get(sum) + 1);
-} else {
-map.put(sum, 1);
-}
-}
-        return ans=0;
+        
+        int ans = 0;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(sum, 1);
+        for(int val: nums) {
+            sum += val;
+
+            if(map.containsKey(sum - k)){
+                ans += map.get(sum - k);
+            }
+
+            if(map.containsKey(sum)){
+                map.put(sum, map.get(sum) + 1);
+            }else{
+                map.put(sum, 1);
+            }
+        }
+        return ans;
     }
 }
