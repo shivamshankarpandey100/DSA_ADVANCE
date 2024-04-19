4e3637ca726fca95a1ca9d34888aeb04bcd3d9b8
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-04-19 20:48:14 +0530

    :fire::fire:X of Kind in a deck of card :fire::fire:

---
+++ HashMap/06.Xofakindinadeckofcards.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        HashMap<Integer, Integer> map =new HashMap<>();
+        for(int val:deck){
+            map.put(val,map.getOrDefault(val,0)+1);
+        }
+
+        int counter=0;
+        int gcd=1;
+
+        for(int key:map.keySet()){
+            if(counter==0){
+                gcd=map.get(key);
+            }else{
+                int num=map.get(key);
+                gcd=getGCD(num,gcd);
+            }
+            counter++;
+        }
+        return gcd>=2;
+    }
+    public int getGCD(int n1, int n2){
+        while(n1%n2!=0){
+            int rem=n1%n2;
+            n1=n2;
+            n2=rem;
+        }
+        return n2;
+
+    }
+    }
