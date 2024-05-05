05e0b27dd8c7488c83ee9cebb1b74c9caca5b5e2
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-05-05 16:17:16 +0530

    :rocket::lock: Find Itinerary from Ticket :lock::rocket:

--- HashMap/22.Find Itinerary from Ticket.java
+++ HashMap/22.Find Itinerary from Ticket.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-class Main{
-	public static void main(String[] args) {
+class Main {
+	public static void main (String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int noofpairs_src_des = scn.nextInt();
 		
