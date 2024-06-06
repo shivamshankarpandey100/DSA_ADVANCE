4a2c74fb4aa4d64f11cdbadd20c0c090e26dfa41
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-06 20:12:39 +0530

    HasMap & HashSet

---
+++ HashMap/44.Count of Substring having at Most K uniqe Chars.java
@@ -0,0 +1,54 @@
+
+
+import java.util.*;
+class Main41 {
+
+	public static int solution(String str, int k) {
+		int ans = 0;
+		int i = -1;
+		int j = -1;
+
+		HashMap<Character, Integer> map = new HashMap<>();
+
+		while(true){
+			
+			while(i < str.length() - 1){
+				i++;
+				char ch = str.charAt(i);
+				map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+				if(map.size() <= k){
+					ans += i - j;
+				}else{
+					break;
+				}
+			}
+
+			if(i == str.length() - 1 && map.size() <= k){
+				break;
+			}
+
+			while(j < i){
+				j++;
+				char ch = str.charAt(j);
+				if(map.get(ch) == 1){
+					map.remove(ch);
+				}else{
+					map.put(ch, map.get(ch) - 1);
+				}
+
+				if(map.size() > k){
+					continue;
+				}else {
+					ans += i - j;
+					break;
+				}
+
+	      	}
+
+		}
+		
+		return ans;
+
+	}
+}
