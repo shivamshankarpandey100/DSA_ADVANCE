79ae7d544d3564afb584e5074d7c25fc6fd53f57
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-16 21:00:06 +0530

    Bit-Manipulation

---
+++ Bit-Manipulation/03.Kernighan's Algo. ( No. of 1's bit ) QN191.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Main {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    
+    //write your code here
+
+    int count=0;
+    while(n!=0){
+        int rmsb=n&-n;//-n is use for 2's complement
+        n=n-rmsb;
+        count++;
+    }
+    System.out.println(count);
+    scn.close();
+  }
+
+}
