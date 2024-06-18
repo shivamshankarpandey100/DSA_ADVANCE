7c90f4ac190792fdd4e913276d2635c815e7b7cb
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-18 20:31:59 +0530

    Bit-Manipulation

---
+++ Bit-Manipulation/09.XOR of Sum of all Pairs.java
@@ -0,0 +1,13 @@
+
+class Main{
+
+    public static int solution(int[] arr){
+      
+        int xor = 0;
+        for(int ele : arr){
+            xor = xor ^ ele;
+        }
+       
+       return 2 * xor;
+    }    
+}
