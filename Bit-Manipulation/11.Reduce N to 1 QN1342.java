c81c566d8e5289ccc9bbfb40c71e2b8d91fdd994
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-20 21:38:16 +0530

    Bit-Manipulation

--- Bit-Manipulation/11.Reduce N to 1 QN1342.java
+++ Bit-Manipulation/11.Reduce N to 1 QN1342.java
@@ -1,6 +1,22 @@
 
 
 //  https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+// class Solution {
+//     public int numberOfSteps(int num) {
+//         int step=0;
+//         while(num != 0){
+//             if(num%2==0){
+//                 num/=2;
+//             }
+//             else{
+//                 num--;
+//             }
+//             step++;
+//         }
+//         return step;
+//     }
+// }
+
 
 //  https://practice.geeksforgeeks.org/problems/minimum-steps-to-minimize-n-as-per-given-condition0618/1
 
@@ -26,16 +42,16 @@ class Main {
 
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
   
- int step = 0;
-        while(n != 1){
-           if((n % 2) == 0) n /= 2;       // 4x + 0
-           else if(n == 3) n--;          
-           else if((n % 4) == 1) n--;     // 4x + 1
-           else if((n % 4) == 3) n++;     // 4x + 3
+//  int step = 0;
+//         while(n != 1){
+//            if((n % 2) == 0) n /= 2;       // 4x + 0
+//            else if(n == 3) n--;          
+//            else if((n % 4) == 1) n--;     // 4x + 1
+//            else if((n % 4) == 3) n++;     // 4x + 3
 
-           step ++;
-        }
-        return step;
+//            step ++;
+//         }
+//         return step;
 
 
         ///////////////////////////////////////////////////////////////////////
