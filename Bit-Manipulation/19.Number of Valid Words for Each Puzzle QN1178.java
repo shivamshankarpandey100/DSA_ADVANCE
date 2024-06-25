e905e0d03508f15167646870c0ef78b3726edf4a
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-06-25 20:19:58 +0530

    Bit-Manipulation

--- Bit-Manipulation/19.Number of Valid Words for Each Puzzle QN1178.java
+++ Bit-Manipulation/19.Number of Valid Words for Each Puzzle QN1178.java
@@ -24,7 +24,7 @@ class Main{
 
       HashSet<Character> set = new HashSet<>();
 
-      for (char ch : word.toCharArray()) {
+      for (Character ch : word.toCharArray()) {
         if (set.contains(ch)) {
           continue;
         }
@@ -37,14 +37,16 @@ class Main{
     ArrayList<Integer> res = new ArrayList<>();
 
     for (String puzzle : puzzles) {
+      ArrayList<Integer> wordsTocheck = map.get(puzzle.charAt(0));
+
       int pmask = 0;
-      for (char ch : puzzle.toCharArray()) {
+      for (Character ch : puzzle.toCharArray()) {
         int bit = ch - 'a';
         pmask = pmask | (1 << bit);
       }
 
-      char fch = puzzle.charAt(0);
-      int wordsTocheck = map.get(fch);
+      // char fch = puzzle.charAt(0);
+      // int wordsTocheck = map.get(fch);
       int count = 0;
 
       for (int wmask : wordsTocheck) {
@@ -60,3 +62,42 @@ class Main{
   }
 
 }
+
+
+// Time Complexity: O(N * M) where N is the number of words and M is the number of puzzles.
+// HashMap<Integer, Integer> wordMaskCount = new HashMap<>();
+
+//         for (String word : words) {
+//             int mask = 0;
+//             for (char ch : word.toCharArray()) {
+//                 mask |= (1 << (ch - 'a'));
+//             }
+
+//             if (Integer.bitCount(mask) <= 7) {
+//                 wordMaskCount.put(mask, wordMaskCount.getOrDefault(mask, 0) + 1);
+//             }
+//         }
+
+//         List<Integer> res = new ArrayList<>();
+
+//         for (String puzzle : puzzles) {
+//             int pmask = 0;
+//             for (char ch : puzzle.toCharArray()) {
+//                 pmask |= (1 << (ch - 'a'));
+//             }
+
+//             int firstCharMask = 1 << (puzzle.charAt(0) - 'a');
+//             int submask = pmask;
+//             int count = 0;
+
+//             while (submask > 0) {
+//                 if ((submask & firstCharMask) != 0 && wordMaskCount.containsKey(submask)) {
+//                     count += wordMaskCount.get(submask);
+//                 }
+//                 submask = (submask - 1) & pmask;
+//             }
+
+//             res.add(count);
+//         }
+
+//         return res;
