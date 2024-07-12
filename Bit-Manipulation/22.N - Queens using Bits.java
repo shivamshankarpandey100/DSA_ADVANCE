a8680b920c372f2ccad2f4e5916f4c19daab401e
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-07-12 18:07:34 +0530

    Bit-Manipulation

---
+++ Bit-Manipulation/22.N - Queens using Bits.java
@@ -0,0 +1,33 @@
+
+
+
+
+class Main{
+
+  public static void solution(boolean[][] board, int qpsf, int cols, int ndiag, int rdiag, String asf) {
+    // write your code here
+
+    if (qpsf == board.length) {
+      System.out.println(asf + ".");
+      return;
+    }
+
+    for (int i = 0; i < board.length; i++) {
+      if ((cols & (1 << i)) == 0 && (ndiag & (1 << (qpsf + i))) == 0
+          && (rdiag & (1 << (qpsf - i + board.length - 1))) == 0) {
+
+        board[qpsf][i] = true;
+        cols ^= (1 << i);
+        ndiag ^= (1 << (qpsf + i));
+        rdiag ^= (1 << (qpsf - i + board.length - 1));
+
+        solution(board, qpsf + 1, cols, ndiag, rdiag, asf + qpsf + "-" + i + ",");
+
+        board[qpsf][i] = false;
+        cols ^= (1 << i);
+        ndiag ^= (1 << (qpsf + i));
+        rdiag ^= (1 << (qpsf - i + board.length - 1));
+      }
+    }
+  }
+}
