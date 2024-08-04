ae0a1e16858eee76e13f4c2c182edea6bf37aa21
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-08-04 13:25:44 +0530

    Bit-Manipulation

--- Bit-Manipulation/24. Count Set Bits in First N Natural No.java
+++ Bit-Manipulation/24. Count Set Bits in First N Natural No.java
@@ -61,3 +61,46 @@ class Main {
 }
 
 // } Driver Code Ends
+
+
+
+
+//---------------------------------------------------------2nd----------------------------------------------------------------------------
+import java.util.*;
+
+class Main{
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    System.out.println(solution(n));
+
+
+    scn.close();
+  }
+
+  public static int solution(int n){
+    
+    if(n == 0){
+        return 0;
+    }
+
+    int x = biggest2Kapower(n);
+    int bitstill2x = x * (1 << (x - 1));
+    int countofMSBfrom2xToN = n - (1 << x) + 1;
+    int rob = solution(n - (1 << x)); 
+
+    return bitstill2x + countofMSBfrom2xToN + rob;
+  }
+
+  public static int biggest2Kapower(int n){
+    int x = 0;
+
+    while((1 << x) <= n){
+        x++;
+    }
+
+    return x - 1;
+  }
+
+}
