127e534269fedeb74438cd83bab6ce4ae948b501
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-08-28 19:30:58 +0530

    Trees

--- Trees/02. Left View of Binary Tree.java
+++ Trees/02. Left View of Binary Tree.java
@@ -37,3 +37,56 @@ class Tree
       
     }
 }
+
+
+
+
+//<------------------------------------------------------------------------------------2nd leetcode-------------------------------------------------------------------------------->
+
+//  public class TreeNode {
+//      int val;
+//      TreeNode left;
+//       TreeNode right;
+//       TreeNode() {}
+//       TreeNode(int val) { this.val = val; }
+//       TreeNode(int val, TreeNode left, TreeNode right) {
+//           this.val = val;
+//           this.left = left;
+//           this.right = right;
+//       }
+//   }
+ 
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+       List<Integer> ret = new ArrayList<>();
+      
+      if(root == null){
+          return ret;
+      }
+      
+      Queue<TreeNode> q = new LinkedList<>();
+      q.add(root);
+      
+      while(q.size() > 0){
+          int lsize = q.size();
+          
+          for(int i = 0; i < lsize; i++){
+              TreeNode temp = q.remove();
+              
+              if(i == lsize-1){
+                  ret.add(temp.val);
+              }
+              
+              if(temp.left != null){
+                  q.add(temp.left);
+              }
+              
+              if(temp.right != null){
+                  q.add(temp.right);
+              }
+          }
+      }
+      
+      return ret; 
+    }
+}
