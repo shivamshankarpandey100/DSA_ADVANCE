620dc4fdb18f0106259f328c64ed28ebca7df0ab
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-12-03 20:13:10 +0530

    String

---
+++ String/Min Chars to Add for Palindrome.java
@@ -0,0 +1,20 @@
+//https://www.geeksforgeeks.org/problems/minimum-characters-to-be-added-at-front-to-make-string-palindrome/1
+
+class Solution {
+    public static int minChar(String A) {
+        String rev = new StringBuilder(A).reverse().toString();
+        String concat = A + "$" + rev;
+        int[] lps = new int[concat.length()];
+        int y = 0;
+        for (int x = 1; x < concat.length(); x++) {
+            while (y > 0 && concat.charAt(x) != concat.charAt(y)) {
+                y = lps[y - 1];
+            }
+            if (concat.charAt(x) == concat.charAt(y)) {
+                y++;
+            }
+            lps[x] = y;
+        }
+        return A.length() - lps[concat.length() - 1];
+    }
+}
