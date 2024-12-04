4146492811b53585dcd1f1d4453cddb185688e72
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-12-04 19:22:46 +0530

    String

---
+++ String/Strings Rotations of Each Other.java
@@ -0,0 +1,21 @@
+//https://www.geeksforgeeks.org/problems/check-if-strings-are-rotations-of-each-other-or-not-1587115620/1
+
+
+class Solution {
+    public static boolean areRotations(String s1, String s2) {
+         int n=s1.length();
+        for(int i=0;i<n;i++){
+            if(s1.charAt(0)==s2.charAt(i) && (i==0 || s1.charAt(n-1)==s2.charAt(i-1))){
+                if(checkRotation(i,s1,s2,n)) return true;
+            }
+        }
+        return false;
+    }
+    private static boolean checkRotation(int i, String s1, String s2, int n){
+        for(int j=0;j<n;j++){
+            if(s1.charAt(j)!=s2.charAt(i%n)) return false;
+            i++;
+        }
+        return true;
+    }
+}
