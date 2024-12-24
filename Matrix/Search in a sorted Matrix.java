528b6bfd58a6fd0e019cffa980d3a81861082e9f
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-12-24 19:31:56 +0530

    matrix

---
+++ Matrix/Search in a sorted Matrix.java
@@ -0,0 +1,19 @@
+//https://www.geeksforgeeks.org/problems/search-in-a-matrix-1587115621/1
+
+class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        int n = mat.length, m = mat[0].length;
+        int low = 0;
+        int high = n*m-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(mat[mid/m][mid%m] < x){
+                low = mid+1;
+            } else if(mat[mid/m][mid%m] > x){
+                high = mid-1;
+            } else return true;
+        }
+        return false;
+    }
+}
+
