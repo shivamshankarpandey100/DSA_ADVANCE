b11e0d9de29f593bb7d3753248e06bb46f5da43d
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-01-03 18:59:13 +0530

    Array

---
+++ Arrays/Number of Ways to Split Array QN2270.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/number-of-ways-to-split-array/
+
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        int n = nums.length;
+        long totalSum = 0; 
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        long prefixSum = 0;
+        int validSplits = 0;
+
+        for (int i = 0; i < n - 1; i++) { 
+            prefixSum += nums[i];
+            long rightSum = totalSum - prefixSum;
+            if (prefixSum >= rightSum) {
+                validSplits++;
+            }
+        }
+
+        return validSplits;
+    }
+}
