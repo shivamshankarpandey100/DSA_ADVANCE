3881c1782c62fcd93baba25e8d337d287dfc335a
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-01-11 21:07:17 +0530

    String

---
+++ String/Construct K Palindrome Strings QN1400.java
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/construct-k-palindrome-strings/
+
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if (s.length() < k){
+            return false;
+        }
+
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        int oddCount = 0;
+
+        for (int i = 0; i < chars.length;) {
+            char current = chars[i];
+            int count = 0;
+            
+            while (i < chars.length && chars[i] == current) {
+                count++;
+                i++;
+            }
+
+            if (count % 2 != 0){
+                oddCount++;
+            } 
+        }
+
+        return oddCount <= k;
+    }
+}
