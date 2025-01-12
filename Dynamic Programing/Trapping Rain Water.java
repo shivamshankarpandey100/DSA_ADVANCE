52207aaed59792d540f45ada6ed4d5eb7db94116
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-01-12 18:07:39 +0530

    DP

---
+++ Dynamic Programing/Trapping Rain Water.java
@@ -0,0 +1,26 @@
+//https://www.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1
+
+class Solution {
+    public int maxWater(int arr[]) {
+        int n=arr.length;
+        int leftmax[]=new int[n];
+        leftmax[0]=arr[0];
+        for(int i=1;i<n;i++){
+            leftmax[i]=Math.max(leftmax[i-1],arr[i]);
+        }
+        
+        int rightmax[]=new int[n];
+        rightmax[n-1]=arr[n-1];
+        
+        for(int i=n-2;i>=0;i--){
+            rightmax[i]=Math.max(rightmax[i+1],arr[i]);
+        }
+        
+        int trappedwater=0;
+        for(int i=0;i<n;i++){
+            int waterlevel=Math.min(leftmax[i],rightmax[i]);
+            trappedwater+=waterlevel-arr[i];
+        }
+        return trappedwater;
+    }
+}
