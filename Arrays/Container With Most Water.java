56b0ee30678ecd2664e4aeaf416e6b1420c93bd8
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-01-13 20:27:59 +0530

    Array

---
+++ Arrays/Container With Most Water.java
@@ -0,0 +1,21 @@
+//https://www.geeksforgeeks.org/problems/container-with-most-water0535/1
+
+class Solution {
+
+    public int maxWater(int arr[]) {
+        // Code Here
+        int i=0,j=arr.length-1;
+        int max=0;
+        while(i<j){
+           int min=Math.min(arr[i],arr[j]);
+           max=Math.max(max,(min * (j-i)));
+           if(arr[i]<arr[j]){
+               i++;
+           }else{
+               j--;
+           }
+          
+       }
+       return max;
+    }
+}
