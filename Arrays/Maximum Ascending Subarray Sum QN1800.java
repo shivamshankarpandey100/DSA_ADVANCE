e40eb7d6441c972189cabd403880c5aa1ba09cc8
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-02-04 18:56:24 +0530

    Array

---
+++ Arrays/Maximum Ascending Subarray Sum QN1800.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/maximum-ascending-subarray-sum/
+
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int curr = nums[0];
+        int ans = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            curr = nums[i] > nums[i - 1] ? curr + nums[i] : nums[i];
+            ans = Math.max(ans, curr);
+        }
+        
+        return ans;
+    }
+}
