1400f84221bb1e4c13fc5462b359fc6128e705b6
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-02-11 20:06:46 +0530

    String

---
+++ String/Remove All Occurrences of a Substring QN1910.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        StringBuilder sb = new StringBuilder(s);
+        
+        int index = sb.indexOf(part);
+        
+       while (index != -1) {
+            sb.delete(index, index + part.length());
+            index = sb.indexOf(part);
+        }
+        
+        return sb.toString();
+    }
+}
