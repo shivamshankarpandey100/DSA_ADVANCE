26d2bf4c9fc53caf4f04519b64ab20028208c57c
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-02-22 19:19:24 +0530

    String

---
+++ String/Longest valid Parentheses.java
@@ -0,0 +1,42 @@
+//https://www.geeksforgeeks.org/problems/longest-valid-parentheses5657/1
+
+class Solution {
+    static int maxLength(String s) {
+       int n=s.length();
+       int l=0;
+       int r=0;
+       int max=0;
+       for(int  i=0;i<n;i++)
+       {
+           if(s.charAt(i)=='(')
+           l++;
+           if(s.charAt(i)==')')
+           r++;
+           if(l==r)
+           max=Math.max(max,2*l);
+           if(r>l)
+           {
+               l=0;
+               r=0;
+           }
+       }
+       r=0;
+       l=0;
+       for(int i=n-1;i>=0;i--)
+       {
+           if(s.charAt(i)=='(')
+           l++;
+           if(s.charAt(i)==')')
+           r++;
+           if(l==r)
+           max=Math.max(max,2*l);
+           if(r<l)
+           {
+               l=0;
+               r=0;
+           }
+       }
+       return max;
+    }
+}
+
