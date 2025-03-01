2d453c68676008a1b5602ab146c87a36ea071ad4
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-03-01 18:55:18 +0530

    Array

---
+++ Arrays/Apply Operations to an Array QN2460.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/apply-operations-to-an-array/
+
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        
+        int[] result = new int[nums.length];
+        int index = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                result[index++] = num;
+            }
+        }
+        return result;
+    }
+}
