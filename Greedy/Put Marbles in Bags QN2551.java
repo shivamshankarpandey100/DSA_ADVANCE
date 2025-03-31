46c1a9f00a82c86467e7d4dcc1cf8de9ea78cc71
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-03-31 22:21:58 +0530

    Greedy

---
+++ Greedy/Put Marbles in Bags QN2551.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/put-marbles-in-bags/
+
+class Solution {
+    public long putMarbles(int[] weights, int k) {
+        int n = weights.length;
+        int[] adjacentSums = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) adjacentSums[i] = weights[i] + weights[i + 1];
+        Arrays.sort(adjacentSums);
+        long minSum = 0, maxSum = 0;
+        for (int i = 0; i < k - 1; i++) minSum += adjacentSums[i];
+        for (int i = n - 2; i >= n - k; i--) maxSum += adjacentSums[i];
+        return maxSum - minSum;
+    }
+}
