c0883607ec2c2671dc57b014d46664fb04c53961
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-03-31 22:18:56 +0530

    String

---
+++ String/Maximize partitions in a String.java
@@ -0,0 +1,22 @@
+//https://www.geeksforgeeks.org/problems/maximize-partitions-in-a-string/1
+
+class Solution {
+    public int maxPartitions(String s) {
+        int x[] = new int[26];
+        for(int i=0;i<s.length();i++) {
+            x[s.charAt(i)-'a'] = i;
+        }
+        
+        int y = 0;
+        int last = 0;
+        
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            last = Math.max(last, x[ch - 'a']);
+            if(last == i){
+                y++;
+            }
+        }
+        return y;
+    }
+}
