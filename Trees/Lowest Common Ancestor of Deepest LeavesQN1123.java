e52594fd703b14edbb0cefd6da92a1153c47fea5
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-04-04 22:40:35 +0530

    Tree

---
+++ Trees/Lowest Common Ancestor of Deepest LeavesQN1123.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+
+class Solution {
+    TreeNode res;
+    int maxDepth = -1;
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        dfs(root, 0);
+        return res;
+    }
+
+    private int dfs(TreeNode node, int depth) {
+        if (node == null) {
+            maxDepth = Math.max(maxDepth, depth);
+            return depth;
+        }
+
+        int left = dfs(node.left, depth + 1);
+        int right = dfs(node.right, depth + 1);
+        if (left == right && left == maxDepth)
+            res = node;
+        return Math.max(left, right);
+    }
+}
