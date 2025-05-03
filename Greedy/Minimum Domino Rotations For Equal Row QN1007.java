0d78d2becc7042e7091fc6f20405a3657e12d475
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-05-03 12:48:34 +0530

    LinkList

---
+++ Greedy/Minimum Domino Rotations For Equal Row QN1007.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/minimum-domino-rotations-for-equal-row
+
+
+class Solution {
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int rotations = check(tops[0], tops, bottoms);
+        if (rotations != -1 || tops[0] == bottoms[0]) return rotations;
+        return check(bottoms[0], tops, bottoms);
+    }
+
+    private int check(int target, int[] tops, int[] bottoms) {
+        int topRotations = 0;
+        int bottomRotations = 0;
+
+        for (int i = 0; i < tops.length; i++) {
+            if (tops[i] != target && bottoms[i] != target) {
+                return -1; 
+            } else if (tops[i] != target) {
+                topRotations++;
+            } else if (bottoms[i] != target) {
+                bottomRotations++;
+            }
+        }
+
+        return Math.min(topRotations, bottomRotations);
+    }
+}
