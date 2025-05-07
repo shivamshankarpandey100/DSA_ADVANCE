d72faa7ebe3e4fa172b3a3ddaaa3dae7a4cefa92
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-05-07 21:08:05 +0530

    Tree

---
+++ Trees/Root to Leaf Paths.java
@@ -0,0 +1,27 @@
+//https://www.geeksforgeeks.org/problems/root-to-leaf-paths/1
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> Paths(Node root) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> path = new ArrayList<>();
+        solve(root, ans, path );
+        return ans; 
+    }
+    public static void solve(Node root , ArrayList<ArrayList<Integer>> ans,
+    ArrayList<Integer> path){
+        if(root == null)
+            return ;
+        
+        path.add(root.data);
+        
+        if(root.left == null && root.right == null){
+            ans.add(new ArrayList<>(path));
+        }
+        
+         solve(root.left , ans, path);
+         solve(root.right , ans , path);
+         
+         //Backtrack
+         path.remove(path.size() - 1);
+    }
+}
