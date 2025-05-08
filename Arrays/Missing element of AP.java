5453c406e741c759fc6968fbaa7ef9cc84a8a4cb
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-05-08 22:44:26 +0530

    Array

---
+++ Arrays/Missing element of AP.java
@@ -0,0 +1,14 @@
+//https://www.geeksforgeeks.org/problems/missing-element-of-ap2228/1
+
+class Solution {
+    public int findMissing(int[] arr) {
+        int n=arr.length;
+        int diff=arr[1]-arr[0];
+        for(int i=2;i<n;i++)
+        {
+            if(arr[i-1]!=arr[i]-diff)
+            return arr[i]-diff;
+        }
+        return arr[n-1]+diff;
+    }
+}
