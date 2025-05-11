81b93c75694d30a9c8f506fcd5365901bad8f1d4
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-05-11 22:19:08 +0530

    Array

---
+++ Arrays/Three Consecutive Odds QN1550.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/three-consecutive-odds/
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int cnt = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % 2 == 1){
+                cnt++;
+
+                if(cnt == 3){
+                    return true;
+                }
+            }else{
+                cnt = 0;
+            }
+        }
+
+        return false;
+    }
+}
