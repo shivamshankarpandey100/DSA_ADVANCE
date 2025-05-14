f36f01b3d0c67d64685283c368af71106d952f72
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-05-14 15:34:29 +0530

    String

---
+++ String/Look and Say Pattern.java
@@ -0,0 +1,29 @@
+//https://www.geeksforgeeks.org/problems/decode-the-pattern1138/1
+
+class Solution {
+    public String countAndSay(int n) {
+        int num=1;
+        String str=String.valueOf(num);
+        StringBuilder ss=new StringBuilder(str);
+        StringBuilder temp=new StringBuilder();
+        for(int i=1;i<n;i++){
+            int len=ss.length();
+            int j=0;
+            temp=new StringBuilder();
+            while(j<len){
+                char digit=ss.charAt(j);
+                int cnt=1;
+                j++;
+                while(j<len&&ss.charAt(j)==digit){
+                    cnt++;
+                    j++;
+                }
+                temp.append(""+cnt);
+                temp.append(digit);
+            }
+            ss=new StringBuilder(temp);
+        }
+        return new String(ss);
+        
+    }
+}
