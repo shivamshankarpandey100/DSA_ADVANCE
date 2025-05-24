509a01598352f6e19a462419caf544de8d8371e9
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-05-24 21:52:23 +0530

    String

---
+++ String/Sum of all substrings of a number.java
@@ -0,0 +1,19 @@
+//https://www.geeksforgeeks.org/problems/sum-of-all-substrings-of-a-number-1587115621/1
+
+
+class Solution {
+    public static int sumSubstrings(String s) {
+        int n = s.length();
+        long sum = s.charAt(0) - '0';
+        long prev = sum;
+
+        for (int i = 1; i < n; i++) {
+            int digit = s.charAt(i) - '0';
+            prev = prev * 10 + digit * (i + 1);
+            sum += prev;
+        }
+
+        return (int)sum;
+        
+    }
+}
