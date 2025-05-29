b373c1abd9e86332fe354e6130b7cc30204fd80f
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-05-29 11:52:46 +0530

    Tree

---
+++ Trees/Sum of nodes on the longest path.java
@@ -0,0 +1,26 @@
+//https://www.geeksforgeeks.org/problems/sum-of-the-longest-bloodline-of-a-tree/1
+
+class Solution {
+    private static int sum, level;
+    public int sumOfLongRootToLeafPath(Node root) {
+        sum = 0;
+        level = -1;
+        dfs(root, 0, 0);
+        return sum;
+    }
+    
+    private void dfs(Node node, int currSum, int currLevel) {
+        if (node == null) return;
+        
+        currSum += node.data;
+        if (currLevel == level) {
+            sum = Math.max(currSum, sum);
+        } else if (currLevel > level) {
+            level = currLevel;
+            sum = currSum;
+        }
+        
+        dfs(node.left, currSum, currLevel + 1);
+        dfs(node.right, currSum, currLevel + 1);
+    }
+}
