5fccc42868c8972b98237f77d32efad3aa3a2114
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-06-07 23:12:12 +0530

    Array

---
+++ Arrays/Longest Span in two Binary Arrays.java
@@ -0,0 +1,17 @@
+//https://www.geeksforgeeks.org/problems/longest-span-with-same-sum-in-two-binary-arrays5142/1
+
+class Solution {
+    public int longestCommonSum(int[] a1, int[] a2) {
+        int n=a1.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0;
+        map.put(sum,-1);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            sum+=a1[i]-a2[i];
+            if(map.containsKey(sum)) ans=Math.max(ans,i-map.get(sum));
+            else map.put(sum,i);
+        }
+        return ans;
+    }
+}
