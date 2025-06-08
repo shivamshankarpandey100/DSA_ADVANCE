4c08c67ab322fa57e5ea0f40775bcfb5a34b5b82
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-06-08 22:34:13 +0530

    Array

---
+++ Arrays/Lexicographical Numbers QN386.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/lexicographical-numbers/
+
+class Solution {
+    static public List<Integer> lexicalOrder(int n) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            ans.add(i);
+        }
+
+        ans.sort((a, b) -> {
+            String s1 = String.valueOf(a);
+            String s2 = String.valueOf(b);
+            return s1.compareTo(s2);
+        });
+
+        return ans;
+    }
+}
