bcb8b6cb136b8c4f8cb1973b32e6d7a81b833012
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2025-06-09 11:05:53 +0530

    Array

---
+++ Trees/BST with Dead End.java
@@ -0,0 +1,19 @@
+//https://www.geeksforgeeks.org/problems/check-whether-bst-contains-dead-end/1
+
+class Solution
+{   
+    public static boolean isDeadEnd(Node root){
+        return helper(root,1,Integer.MAX_VALUE);
+    }
+    
+    public static boolean helper(Node root,int minVal,int maxVal){
+        if(root==null)return false;
+        
+        if(minVal==maxVal){
+            return true;
+        }
+        
+        return helper(root.left,minVal,root.data-1)||helper(root.right,root.data+1,maxVal);
+    }
+}
+
